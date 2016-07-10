795a36eadd4478f738583b6a3e2ddc09ee5cf1f1
Edwin Torres <edwin.torres@prodigious.com>
2016-07-09 22:30:11 -0500

    Updating Data Binding and Interpolation

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,21 +2,20 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'my-app',
-    template: `<h1>This is Sparta!</h1>
-             <div>
-                <p>Welcome to the parche <b>{{student.name}}</b></p>
-                <p>You are <b>{{student.age}}</b> years old</p>
-                <p>And your favorite color is <b [style.color]="student.favoriteColor">{{student.favoriteColor}}</b></p>
-             </div>
-             <div>
-                This is the whole object {{student | json}}
-</div>
-             `
+    templateUrl: './app/templates/main.html'
 })
 export class AppComponent{
     student = {
         "name": "Edwin Torres",
         "age": 27,
         "favoriteColor": "red"
+    }
+
+    myClick(){
+        alert("Me diste click :D");
+    }
+
+    myBlur(){
+        console.log("Saliste!");
     }
 }
